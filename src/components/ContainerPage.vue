28ff98da8d373ca44c91daeca94d1d1c6621495e
Murilo Pereira <muriloedu006@gmail.com>
2022-06-02 22:38:20 -0300

    fix: eslint

--- src/components/ContainerPage.vue
+++ src/components/ContainerPage.vue
@@ -8,16 +8,24 @@ const error = ref(null);
 
 fetch(url)
   .then((res) => res.json())
-  .then((json) => (data.value = json))
-  .catch((err) => (error.value = err));
+  .then((json) => {
+    data.value = json;
+    return data;
+  })
+  .catch((err) => {
+    error.value = err;
+    return error;
+  });
 </script>
 
 <template>
-  <input type="text" placeholder="Search" key="search-pokemon" v-model="searchPokemon"/>
+  <label for="searchPokemon">
+    <input type="text" placeholder="Search" key="search-pokemon" v-model="searchPokemon"/>
+  </label>
   <div v-if="error">Oops! Error encountered: {{ error.message }}</div>
   <div v-else-if="data">
     <div class="pokemonBox" v-for="(pokemon, key) in data.results" :key="key">
-      <div v-if="pokemon.name.toLowerCase().includes(searchPokemon)">
+      <div v-if="pokemon.name.toLowerCase().includes(searchPokemon.toLowerCase())">
         <PokemonDiv :pokemon="pokemon" :num="key + 1" />
       </div>
     </div>
@@ -31,7 +39,7 @@ export default {
   components: {
     PokemonDiv,
   },
-  data: function () {
+  data() {
     return {
       searchPokemon: '',
     };
